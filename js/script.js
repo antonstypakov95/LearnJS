7d8f250554175d3788432b5ff5a29600f7629c58
Anton <stanton5991al@gmail.com>
2024-04-11 21:02:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+'use strict'
+// let userName = 'John';
+// let userNumber = 25;
+// userNumber = 10;
+// console.log(userName);
+
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18 лет', '');
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your last name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/car/6`);
+// const userName = 'Anton';
+// alert(`Hello, ${userName}`);
+
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(25%7);
+
+&& /* и( и то и то правда(true)) */
+|| /* или (одно из правда)*/
+= /* присваивание */
+== /* равенство(сравнение) */
+=== /* строгое равенство( сравнение по типу typeof, число = числу) */
+!= /* неравенство (не равно, без учета типа) */
+!== /* строгое неравенство (учитывается сравнение по типу typeof) */
+++incr, incr++ /*инкримент - увеличение на единицу(1), плюсы(++)перед командой,называется префиксным и увеличивает сразу, плюсы после команды - постфиксным и сначала выводит предыдущее число, а после увеличивает  */
+--decr, decr-- /* декримент - уменьшение на единицу(1) */
