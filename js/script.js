a14c000eb2c48312e8ffb67cb1c48795b75145f9
Anton <stanton5991al@gmail.com>
2024-04-14 21:44:50 +0300

    first console app

--- js/script.js
+++ js/script.js
@@ -1,54 +1,21 @@
 'use strict'
-// let userName = 'John';
-// let userNumber = 25;
-// userNumber = 10;
-// console.log(userName);
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-// alert('Hello');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// const answer = +prompt('Вам есть 18 лет', '');
-// console.log(answer + 5);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-// const answers = [];
-
-// answers[0] = prompt('What is your name?', '');
-// answers[1] = prompt('What is your last name?', '');
-// answers[2] = prompt('How old are you?', '');
-
-// console.log(typeof(answers));
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/car/6`);
-// const userName = 'Anton';
-// alert(`Hello, ${userName}`);
-
-
-// let incr = 10,
-//     decr = 10;
-
-// console.log(incr++);
-// console.log(decr--);
-
-// console.log(incr);
-// console.log(decr);
-
-// console.log(++incr);
-// console.log(--decr);
-
-// console.log(25%7);
-
-&& /* и( и то и то правда(true)) */
-|| /* или (одно из правда)*/
-= /* присваивание */
-== /* равенство(сравнение) */
-=== /* строгое равенство( сравнение по типу typeof, число = числу) */
-!= /* неравенство (не равно, без учета типа) */
-!== /* строгое неравенство (учитывается сравнение по типу typeof) */
-++incr, incr++ /*инкримент - увеличение на единицу(1), плюсы(++)перед командой,называется префиксным и увеличивает сразу, плюсы после команды - постфиксным и сначала выводит предыдущее число, а после увеличивает  */
---decr, decr-- /* декримент - уменьшение на единицу(1) */
+console.log(personalMovieDB);
